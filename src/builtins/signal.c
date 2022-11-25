4403218e0e566db2bad7bd45aabece846bf5606a
Michela Volpi <mvolpi@e4r14p4.42roma.it>
2022-11-25 10:59:15 +0100

    tolti i punti 0

--- src/builtins/signal.c
+++ src/builtins/signal.c
@@ -6,16 +6,19 @@
 /*   By: mvolpi <mvolpi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 09:27:35 by mvolpi            #+#    #+#             */
-/*   Updated: 2022/11/25 09:46:20 by mvolpi           ###   ########.fr       */
+/*   Updated: 2022/11/25 10:57:51 by mvolpi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../h_file/minishell.h"
 
+// void	rl_replace_line(const char *text, int clear_undo);
+
 void	handle_sigint(int sig)
 {
 	(void)sig;
 	ft_printf("\n");
+	// rl_replace_line("", 0);
 	rl_on_new_line();
 	rl_redisplay();
 }
